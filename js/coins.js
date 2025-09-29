8bece99712bfbfaf28e58689dfd917edb2abc4ed
zicyrz <zicseven@gmail.com>
2025-09-29 15:15:32 +0700

    coins.js

--- js/coins.js
+++ js/coins.js
@@ -1,42 +1,28 @@
-// coins.js
 function createCoin() {
   const coin = document.createElement("img");
   coin.src = "https://s2.coinmarketcap.com/static/img/coins/128x128/1.png"; // logo BTC
-  coin.className = "falling-coin";
+  coin.classList.add("coin");
 
-  // posisi random
+  // posisi random di layar (horizontal)
   coin.style.left = Math.random() * window.innerWidth + "px";
-  coin.style.top = "-50px";
-  coin.style.width = "30px";
 
   document.body.appendChild(coin);
 
-  // jatuh
-  let speed = 2 + Math.random() * 3;
-  let angle = Math.random() * 360;
-
-  function fall() {
-    let top = parseFloat(coin.style.top);
-    if (top < window.innerHeight) {
-      coin.style.top = top + speed + "px";
-      coin.style.transform = `rotate(${angle}deg)`;
-      angle += 5;
-      requestAnimationFrame(fall);
-    } else {
-      coin.remove();
-    }
-  }
-  fall();
+  // hapus setelah animasi selesai (10 detik biar gak numpuk)
+  setTimeout(() => {
+    coin.remove();
+  }, 10000);
 }
 
-// bikin banyak koin tiap 30 detik
-setInterval(() => {
+function coinRain() {
+  // bikin 50 coin sekaligus
   for (let i = 0; i < 50; i++) {
-    setTimeout(createCoin, i * 200); // delay biar nggak jatuh barengan
+    setTimeout(createCoin, i * 200); // jeda antar coin biar ga barengan banget
   }
-}, 30000);
-
-// pas load pertama langsung jatuh juga
-for (let i = 0; i < 50; i++) {
-  setTimeout(createCoin, i * 200);
 }
+
+// jalankan pertama kali
+coinRain();
+
+// ulangi setiap 30 detik
+setInterval(coinRain, 30000);
